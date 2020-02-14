9d4340df5ab21d7368fa5b44ce5c03b633025f00
SabinaSadowska <sadowwwska@gmail.com>
2020-02-14 16:48:41 +0100

    input value to localStorage works fine, email validation added as well

---
+++ landing-page/scripts/form.js
@@ -0,0 +1,32 @@
+const button = document.querySelector(".form__button");
+const input = document.querySelector(".form__input");
+
+
+/* on preventDefault html email validation doesn't work, solution from:
+https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+*/
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// our code:
+
+function emailToStorage(event) {
+    event.preventDefault();
+    let inputValue = input.value;
+    if (!inputValue) {
+        alert("podaj maila")
+    }
+    let isEmail = validateEmail(inputValue);
+    if (!isEmail) {
+        alert("Adres email jest niepoprawny.")
+    } else {
+        localStorage.setItem("email", inputValue);
+    } 
+}
+
+button.addEventListener("click", emailToStorage);
+
+
