a1c6af692ea0d8ed9b9271e6fd69798fb114b714
Robert Pasieczny <pasrobert@icloud.com>
2020-02-21 16:49:36 +0100

    some-problems-solved

--- scripts/nav-backlight.js
+++ scripts/nav-backlight.js
@@ -1,20 +1,27 @@
-(function() {
+(function () {
 
   const sectionsEl = document.querySelectorAll("section");
   const sections = {};
-  
-  sectionsEl.forEach(function(element) {
+
+  sectionsEl.forEach(function (element) {
     sections[element.id] = element.offsetTop;
   });
 
-  window.addEventListener("scroll" ,function() {
+  window.addEventListener("scroll", function () {
     const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
     for (let i in sections) {
-      if (sections[i] <= scrollPosition+40) {
+      if (sections[i] <= scrollPosition + 40) {
         document.querySelector('.active').classList.remove("active");
-        document.querySelector('a[href*=' + i + ']').classList.add('active');      
+        document.querySelector('a[href*=' + i + ']').classList.add('active');
+      if (i === "hero") {
+          document.querySelector('a[href*=top]').classList.add('active');
+      } else {
+          document.querySelector('a[href*=top]').classList.remove('active');
+        }
       }
     }
   });
 })();
+
+
 
