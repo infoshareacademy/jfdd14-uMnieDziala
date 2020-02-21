ff6f21d46b59aa2367dfde1e2f0a46745e0d5c27
SabinaSadowska <sadowwwska@gmail.com>
2020-02-21 15:57:09 +0100

    onscroll animation js fixed

--- scripts/about-us-animation.js
+++ scripts/about-us-animation.js
@@ -2,8 +2,17 @@ const aboutUsSection = document.getElementById('about');
 const pictures = document.querySelectorAll(".animation__container__inner")
 
 const animatePictures = function(){
-    for (let i = 0; i<4; i++)
-    pictures[i].classList.add("pictures-animation")
+
+    if (window.scrollY + window.innerHeight> aboutUsSection.offsetTop + aboutUsSection.offsetHeight * 2/3) {
+        for (let i = 0; i<4; i++) {
+            setTimeout(function() {
+                pictures[i].classList.add("pictures-animation")
+            }, 200*i);
+            
+        }
+
+        window.removeEventListener("scroll", animatePictures); 
+    }
 }
 
-aboutUsSection.addEventListener("scroll", animatePictures()); 
+window.addEventListener("scroll", animatePictures); 
