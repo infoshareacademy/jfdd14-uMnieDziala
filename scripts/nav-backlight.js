9ea95c982676c32f363c5727965ae3ebf3331fff
Robert Pasieczny <pasrobert@icloud.com>
2020-02-24 14:30:46 +0100

    commit before pull

--- scripts/nav-backlight.js
+++ scripts/nav-backlight.js
@@ -9,6 +9,7 @@
 
   window.addEventListener("scroll", function () {
     const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    const 
     for (let i in sections) {
       if (sections[i] <= scrollPosition + 40) {
         document.querySelector('.active').classList.remove("active");
