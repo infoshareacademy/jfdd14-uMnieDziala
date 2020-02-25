599017bfb194b48d9c95deec2e09e9a3abf0e698
Julia <julialojek@gmail.com>
2020-02-25 16:49:59 +0100

    enemies creating and moving

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,5 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-
 const scale = 50;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
@@ -14,7 +13,7 @@ class Enemy {
     color = "white";
     x = (Math.floor(Math.random()*columns - 1) + 1)*scale;
     y = 0;
-    velY = 0;
+    velY = scale;
     create = () => {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -24,4 +23,24 @@ class Enemy {
     };
 }
 
-const mewa = new Enemy;
+const enemies = [];
+
+function newEnemies () {
+    window.setInterval ( () => {
+        enemies.push(new Enemy);
+        enemies.push(new Enemy);
+    }, 3000 );
+}
+
+function moving () {
+    window.setInterval ( () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        enemies.forEach( el => {
+            el.create();
+            el.update();
+        } )
+    }, 1000 );
+}
+
+newEnemies();
+moving();
