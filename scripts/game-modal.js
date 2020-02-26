6838e1846de742d1dc79fbeefc049b237f6f5e35
Sabina Sadowska <sadowwwska@gmail.com>
2020-02-26 22:32:30 +0100

    quit btn done

---
+++ scripts/game-modal.js
@@ -0,0 +1,11 @@
+const modalWIndow = document.querySelector('.game-modal');
+const btnQuit = document.querySelector('.game-modal__btn--quit');
+const btnPlay = document.querySelector('.game-modal__btn--play');
+
+function closeModal(){
+    modalWIndow.classList.remove('game-modal--active')
+}
+
+
+
+btnQuit.addEventListener('click', closeModal)
