4f9df9ce7437413c5582effa4da6132c72113145
Robert Pasieczny <pasrobert@icloud.com>
2020-02-27 15:10:24 +0100

    player-movement-is-working

--- scripts/game.js
+++ scripts/game.js
@@ -6,64 +6,48 @@ const columns = canvas.width / scale;
 const startButton = document.querySelector("button");
 
 class Swimmer {
-    // pozycja głowy
-    x = Math.floor(columns/2) * scale ;
-    y = 0;
-    // prędkosć głowy
-    velX = scale * 1;
-    velY = 0;
-    // kolor węa
-    color = "#fff";
-    // długość ogona (czyli długość węza nie licząc głowy)
-    total = 0;
-    // elementy ogona, kazdy z nich ma x i y
-    tail = [];
+    constructor() {
+        this.draw();
+     }
 
+    width = scale;
+    height = scale;
+    x = Math.floor(columns/2) * scale ;
+    y = canvas.height - (this.height * 2);
+    velX = 0;
+    velY = 0;
+    color = "red";
 
     draw = () => {
         ctx.fillStyle = this.color;
-        // Rysujemy kazdy element ogona (bez głowy!)
-        this.tail.forEach(item => ctx.fillRect(item.x, item.y, scale, scale));
-        // Rysujemy głowę
         ctx.fillRect(this.x, this.y, scale, scale);
     }
 
     update = () => {
-        // Kazdemu elementowi ogona przypisujemy pozycję następnego elementu ogona (od końcówki do początku ogona, ale od początku do końcówki tablicy!)
-        this.tail.forEach((item, i) => {
-            if (i < this.tail.length - 1) {
-                this.tail[i] = this.tail[i + 1];
-            }
-        });
-
-        // Do ostatniego elementu tablicy (element ogona przy głowie), przypisujemy wartość pozycji głowy
-        this.tail[this.total - 1] = { x: this.x, y: this.y };
-
-        // Zmieniamy pozycję głowy (teraz juz mozemy, bo została przypisana do części ogona zaraz za głową)
         this.x += this.velX;
         this.y += this.velY;
     }
 
-    changeDir = (btn) => {
-        switch (btn) {
-            case "w":
-                this.velX = 0;
-                this.velY = -(scale * 1);
-                break;
-            case "s":
-                this.velX = 0;
-                this.velY = scale * 1;
-                break;
-            case "a":
-                this.velX = -(scale * 1);
-                this.velY = 0;
-                break;
-            case "d":
-                this.velX = scale * 1;
-                this.velY = 0;
-                break;
-        }
-    }
+    // changeDir = (btn) => {
+    //     switch (btn) {
+    //         case "w":
+    //             this.velX = 0;
+    //             this.velY = -(scale * 1);
+    //             break;
+    //         case "s":
+    //             this.velX = 0;
+    //             this.velY = scale * 1;
+    //             break;
+    //         case "a":
+    //             this.velX = -(scale * 1);
+    //             this.velY = 0;
+    //             break;
+    //         case "d":
+    //             this.velX = scale * 1;
+    //             this.velY = 0;
+    //             break;
+    //     }
+    // }
 }
 
 const swimmer = new Swimmer();
@@ -109,31 +93,16 @@ function moving () {
     moveEnemies = setInterval ( () => {
         console.log(enemies);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        swimmer.draw();
+        swimmer.update();
         enemies.forEach( el => {
             el.create();
             el.update();
-            swimmer.update();
         } )
 
     }, 100 );
 }
 
-<<<<<<< HEAD
-function setup() {
-    // Dodanie event listenera na wciśnięcie przycisku
-    window.addEventListener("keydown", event => swimmer.changeDir(event.key));
-
-    // Nasza "klatka" gry, elementy które mają wykonać się cyklicznie są wkładane tutaj
-        window.setInterval(() => {
-        swimmer.update();
-        swimmer.draw();
-    }, 250);
-}
-
-newEnemies();
-moving();
-setup();
-=======
 startButton.addEventListener("click", () => {
     clearInterval(startGame);
     clearInterval(moveEnemies);
@@ -142,4 +111,19 @@ startButton.addEventListener("click", () => {
     newEnemies();
     moving();
 } );
->>>>>>> 42a972172c89f43baf3814978fddfcbd7a2d0d1c
+
+window.addEventListener('keydown', function(event){
+    if( event.key === "ArrowLeft" ){
+        swimmer.velY = 0;
+        swimmer.velX = (scale * -1) / 4 ;
+    } else if ( event.key === "ArrowRight" ){
+        swimmer.velY = 0;
+        swimmer.velX = (scale * 1) / 4;
+    } else if ( event.key === "ArrowUp" ){
+        swimmer.velX = 0;
+        swimmer.velY = (scale * -1) / 4;
+    } else if ( event.key === "ArrowDown" ){
+        swimmer.velX = 0;
+        swimmer.velY = (scale * 1) / 4;
+    }
+})
