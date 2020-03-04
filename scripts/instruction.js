b5e7d81fa3eebc034dca3d6906234adea5cda385
Robert Pasieczny <pasrobert@icloud.com>
2020-03-04 18:36:15 +0100

    good-version-before-merge

--- scripts/instruction.js
+++ scripts/instruction.js
@@ -1,25 +1,17 @@
-// Get the modal
 const modal = document.getElementById("modal__instruction");
-console.log(modal)
 
-// Get the button that opens the modal
 const btn = document.getElementById("btn__instruction");
-console.log(btn);
 
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
