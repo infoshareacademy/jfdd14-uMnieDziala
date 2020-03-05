9d2373238b6e021ec756197dc7205f2d3e384f76
Robert Pasieczny <pasrobert@icloud.com>
2020-03-05 12:52:10 +0100

    changes-after-CR

--- scripts/instruction.js
+++ scripts/instruction.js
@@ -1,19 +1,17 @@
 const modal = document.getElementById("modal__instruction");
-
 const btn = document.getElementById("btn__instruction");
-
 const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.addEventListener("click", function() {
   modal.style.display = "block";
-}
+});
 
-span.onclick = function() {
+span.addEventListener("click", function() {
   modal.style.display = "none";
-}
+});
 
-window.onclick = function(event) {
+window.addEventListener("click", function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+});
